7ea4ad68496dee0b65ea890dee93ba0bcace52a9
nimonian <nimon@hotmail.co.uk>
2024-01-26 09:25:49 +0000

    clean up sidebar

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vitepress'
+import * as sidebars from './sidebars/index.js'
 
 export default defineConfig({
-  title: 'Corndel SWE',
-  description: 'for software apprenticeships',
+  title: 'Corndel',
+  description: 'for digital apprenticeships',
   srcDir: 'src',
   themeConfig: {
     logo: '/logo.png',
@@ -45,95 +46,7 @@ export default defineConfig({
     ],
 
     sidebar: {
-      '/js/': [
-        {
-          text: 'Introduction'
-        },
-        {
-          text: 'Pre-reading',
-          collapsed: true,
-          items: [
-            { text: 'Variables' },
-            { text: 'Operators and expressions' },
-            { text: 'Data types' },
-
-            { text: 'Conditional statements' },
-            { text: 'While loops' },
-            { text: 'For loops' }
-          ]
-        },
-
-        {
-          text: 'Fundamentals',
-          collapsed: true,
-          items: [
-            // W1D1
-            { text: 'Functions' },
-            { text: 'Parameters' },
-            { text: 'Composing functions' },
-
-            // W1D2
-            { text: 'Arrays' },
-            { text: 'Strings' },
-            { text: 'Iteration' },
-
-            // W1D3
-            { text: 'Objects' },
-            { text: 'Methods' },
-            { text: 'Error handling' }
-          ]
-        },
-
-        {
-          text: 'Testing',
-          collapsed: true,
-          items: [
-            // W2D1
-            { text: 'Unit testing with Jest' },
-            { text: 'Jest features' },
-            { text: 'TDD' }
-          ]
-        },
-
-        {
-          text: 'OOP',
-          collapsed: true,
-          items: [
-            // W2D2
-            { text: 'Classes' },
-            { text: 'Inheritance' },
-            { text: 'Object oriented design' },
-
-            // W3D3
-            { text: 'Static' },
-            { text: 'Private' },
-            { text: 'Getters and setters' }
-          ]
-        },
-
-        {
-          text: 'Asynchronous code',
-          collapsed: true,
-          items: [
-            { text: 'Callbacks' },
-            { text: 'Promises' },
-            { text: 'async/await' },
-
-            {
-              text: 'Reading and writing files',
-              link: '/js/reading-and-writing-files.md'
-            },
-            {
-              text: 'Working with JSON',
-              link: '/js/working-with-json.md'
-            },
-            {
-              text: 'Fetching data',
-              link: '/js/fetching-data.md'
-            }
-          ]
-        }
-      ]
+      '/js/': sidebars.javascript
     },
 
     socialLinks: [
---
+++ .vitepress/sidebars/javascript.js
@@ -0,0 +1,100 @@
+export const javascript = [
+  {
+    text: 'Introduction'
+  },
+  {
+    text: 'Pre-reading',
+    // collapsed: true,
+    items: [
+      { text: 'Variables' },
+      { text: 'Operators and expressions' },
+      { text: 'Data types' },
+
+      { text: 'Logical operators' },
+      { text: 'Conditional statements' },
+
+      { text: 'Functions' },
+      { text: 'Parameters' }
+    ]
+  },
+
+  {
+    text: 'Fundamentals',
+    // collapsed: true,
+    items: [
+      { text: 'Working with arrays', link: '/js/working-with-arrays' },
+      { text: 'Array methods', link: '/js/array-methods' },
+      { text: 'String methods', link: '/js/string-methods' },
+
+      { text: 'While loops' },
+      { text: 'For loops' },
+      { text: 'Iteration' },
+
+      { text: 'Objects as key value stores' },
+      { text: 'Object methods' }
+    ]
+  },
+
+  {
+    text: 'Frameworks',
+    // collapsed: true,
+    items: [
+      { text: 'Creating a node project' },
+      { text: 'Importing and exporting' },
+      { text: 'Managing dependencies' },
+      { text: 'Command line basics' },
+      { text: 'Making a CLI' },
+      { text: 'CLI user experience' },
+      { text: 'Catching errors' },
+      { text: 'Throwing errors' }
+    ]
+  },
+
+  {
+    text: 'OOP',
+    // collapsed: true,
+    items: [
+      { text: 'Classes and instances' },
+      { text: 'Methods' },
+      { text: 'Getters and setters' },
+      { text: 'Private properties' },
+      { text: 'Static properties' },
+      { text: 'Inheritance' },
+      { text: 'Overriding and customisation' }
+    ]
+  },
+
+  {
+    text: 'Testing',
+    // collapsed: true,
+    items: [
+      { text: 'Writing unit tests' },
+      { text: 'Testing best practices' },
+      { text: 'Mocha features' },
+      { text: 'TDD' }
+    ]
+  },
+
+  {
+    text: 'Asynchronous code',
+    // collapsed: true,
+    items: [
+      { text: 'Callbacks' },
+      { text: 'Promises' },
+      { text: 'async/await' },
+
+      {
+        text: 'Reading and writing files',
+        link: '/js/reading-and-writing-files.md'
+      },
+      {
+        text: 'Working with JSON',
+        link: '/js/working-with-json.md'
+      },
+      {
+        text: 'Fetching data',
+        link: '/js/fetching-data.md'
+      }
+    ]
+  }
+]
