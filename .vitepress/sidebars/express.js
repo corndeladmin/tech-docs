d67af441ef4e1650d8fa4823b53af18f609f5687
Simon Haines <nimon@hotmail.co.uk>
2024-04-10 09:29:51 +0100

    fixes sidebar

--- .vitepress/sidebars/express.js
+++ .vitepress/sidebars/express.js
@@ -19,36 +19,8 @@ export const express = [
   },
   {
     text: 'Creating an API',
-    text: 'Data layer',
-    items: [
-      {
-        text: 'Managing the database',
-        link: '/express/managing-the-database'
-      },
-      {
-        text: 'Connecting to a database',
-        link: '/express/connecting-to-a-database'
-      },
-      {
-        text: 'Adding a model layer',
-        link: '/express/adding-a-model-layer'
-      }
-    ]
-  },
-  {
-    text: 'Creating an API',
     items: [
       { text: 'Creating a server', link: '/express/creating-a-server' },
-      {
-        text: 'Request and response',
-        link: '/express/request-response',
-        collapsed: true,
-        items: [
-          { text: 'Query params', link: '/express/query-params' },
-          { text: 'URL params', link: '/express/url-params' },
-          { text: 'Body and headers', link: '/express/body-and-headers' }
-        ]
-      },
       {
         text: 'Request and response',
         link: '/express/request-response',
@@ -62,17 +34,6 @@ export const express = [
       { text: 'Routing', link: '/express/routing' },
       { text: 'Sending errors', link: '/express/sending-errors' },
       { text: 'Schema validation', link: '/express/schema-validation' }
-    ]
-  },
-  {
-    text: 'Server side rendering',
-    items: [
-      { text: 'Static files' },
-      { text: 'Views and templates' },
-      { text: 'Partial templates' },
-      { text: 'Styling with CSS' },
-      { text: 'Rendering lists' },
-      { text: 'User input' }
     ]
   },
   {
