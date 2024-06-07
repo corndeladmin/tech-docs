c493e1e13b414c553dce93b3a7d4969634b6f721
Simon Haines <nimon@hotmail.co.uk>
2024-06-07 17:49:41 +0100

    adds variables and nav

---
+++ .vitepress/sidebars/java.js
@@ -0,0 +1,15 @@
+export const java = [
+  {
+    text: 'Introduction',
+    link: '/java/'
+  },
+  {
+    text: 'Installation',
+    link: '/java/installation'
+  },
+  {
+    text: 'Foundations',
+    collapsed: true,
+    items: [{ text: 'Variables', link: '/java/variables' }]
+  }
+]
